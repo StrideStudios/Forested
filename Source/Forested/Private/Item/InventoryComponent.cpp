4445d2cbef93d3f6392a4a85f9d1ab1e06752824
StrideStudios <StrideStudios868@gmail.com>
2025-01-28 22:33:48 -0500

    removed more cached files lol

--- Source/Forested/Private/Item/InventoryComponent.cpp
+++
@@ -1,7 +0,0 @@
-#include "Item/InventoryComponent.h"
-
-void UInventoryComponent::BeginPlay() {
-	Super::BeginPlay();
-	//reserve memory for items to reduce lag from allocating memory
-	Items.Reserve(Capacity);
-}
